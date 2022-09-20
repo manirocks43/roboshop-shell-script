24d5bcf4faa9c6418e09903f886792bec70daf7e
ACER <manirocks43@gmail.com>
2022-09-19 23:26:03 -0400

    Mongodb server

--- mongodb.sh
+++ mongodb.sh
@@ -2,12 +2,14 @@ LOG_FILE=/tmp/mongodb
 
 echo "Setting up mongodb service"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG_FILE
+echo Status = $?
 
-echo"installing mongodb service"
+echo "installing mongodb service"
 yum install -y mongodb-org &>>$LOG_FILE
+echo Status = $?
 
 systemctl enable mongod
-systemctl start mongod
+systemctl start mongodx``
 
 
 
