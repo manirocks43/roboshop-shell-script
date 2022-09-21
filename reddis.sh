59dfd2af02ce2c16518f15e9e673e61ceaa35412
ACER <manirocks43@gmail.com>
2022-09-21 19:04:33 -0400

    Redis server

--- reddis.sh
+++ reddis.sh
@@ -3,23 +3,23 @@ LOG_FILE=/tmp/redis
 source common.sh
 
 echo "Setup YUM Repos for Redis"
-dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>/tmp/redis
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>LOG_FILE
 StatusCheck $?
 
 echo "enabling Redis YUM module"
-dnf module enable redis:remi-6.2 -y &>>/tmp/redis
+dnf module enable redis:remi-6.2 -y &>>LOG_FILE
 StatusCheck $?
 
 echo "Installing Redis"
-yum install redis -y &>>/tmp/redis
+yum install redis -y &>>LOG_FILE
 StatusCheck $?
 
 echo"Updating Redis listen address from 127.0.0.1 to 0.0.0.0"
 sed -i -e 's/127.0.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf
 StatusCheck $?
 
-systemctl enable redis &>>/tmp/redis
+systemctl enable redis &>>LOG_FILE
 
 echo "Start redis"
-systemctl restart redis &>>/tmp/redis
+systemctl restart redis &>>LOG_FILE
 StatusCheck $?
