72e9f48cf9f936dbeaee8cf587243688fedf04fb
ACER <manirocks43@gmail.com>
2022-09-20 22:28:17 -0400

    Functions

--- catalogue.sh
+++ catalogue.sh
@@ -1,21 +1,7 @@
 LOG_FILE=/tmp/catalogue
 ID=$(id -u)
 
-if [ $ID -ne 0 ]
-  then
-  echo You should run this script as a root user or with sudo privilages
-  exit 1
-fi
-
-StatusCheck () {
-  if [ $1 -eq 0 ]
-    then
-    echo -e Status = "\e[32mSUCCESS\e[0m"
-    else
-    echo -e Status = "\e[31mFAILURE\e[0m"
-    exit 1
-  fi
-}
+source common.sh
 
 echo "setup nodejs repo"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOG_FILE
--- mongodb.sh
+++ mongodb.sh
@@ -2,40 +2,40 @@ LOG_FILE=/tmp/mongodb
 
 echo "Setting up mongodb service"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG_FILE
-echo Status = $?
+StatusCheck $?
 
 echo "installing mongodb service"
 yum install -y mongodb-org &>>$LOG_FILE
-echo Status = $?
+StatusCheck $?
 
 echo "Updating mongodb listen address"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-echo Status = $?
+StatusCheck $?
 
 echo "Starting mongodb service"
 systemctl enable mongod &>>$LOG_FILE
 systemctl restart mongod &>>$LOG_FILE
-echo Status = $?
+StatusCheck $?
 
 echo "Downloading mongodb schema file"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>$LOG_FILE
-echo Status = $?
+StatusCheck $?
 
 cd /tmp
 
 echo "Unzipping schema file"
 unzip mongodb.zip &>>$LOG_FILE
-echo Status = $?
+StatusCheck $?
 
 cd mongodb-main
 
 echo "Load catalogue service schema"
 mongo < catalogue.js &>>$LOG_FILE
-echo Status = $?
+StatusCheck $?
 
 echo "Load user service schema"
 mongo < users.js &>>$LOG_FILE
-echo Status = $?
+StatusCheck $?
 
 
 
