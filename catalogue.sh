f169c1576f2e114c46928ccc779f98473228081b
ACER <manirocks43@gmail.com>
2022-09-20 21:28:56 -0400

    IF condition

--- catalogue.sh
+++ catalogue.sh
@@ -12,7 +12,7 @@ echo "setup nodejs repo"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOG_FILE
 if [ $? -eq 0 ]
   then
-  echo -e Status = "\2[32mSUCCESS\e[0m"
+  echo -e Status = "\e[32mSUCCESS\e[0m"
   else
   echo -e Status = "\e[31mFAILURE\e[0m"
   exit 1
@@ -22,7 +22,7 @@ echo "installing nodejs repo"
 yum install nodejs -y &>>$LOG_FILE
 if [ $? -eq 0 ]
   then
-  echo -e Status = "\2[32mSUCCESS\e[0m"
+  echo -e Status = "\e[32mSUCCESS\e[0m"
   else
   echo -e Status = "\e[31mFAILURE\e[0m"
   exit 1
@@ -34,7 +34,7 @@ if [ $? -ne 0 ] ; then
   useradd roboshop &>>$LOG_FILE
   if [ $? -eq 0 ]
     then
-    echo -e Status = "\2[32mSUCCESS\e[0m"
+    echo -e Status = "\e[32mSUCCESS\e[0m"
     else
     echo -e Status = "\e[31mFAILURE\e[0m"
     exit 1
@@ -45,19 +45,20 @@ echo "downloading catalogue application"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG_FILE
 if [ $? -eq 0 ]
   then
-  echo -e Status = "\2[32mSUCCESS\e[0m"
+  echo -e Status = "\e[32mSUCCESS\e[0m"
   else
   echo -e Status = "\e[31mFAILURE\e[0m"
   exit 1
 fi
 
 cd /home/roboshop
+rm -rf catalogue &>>$LOG_FILE
 
 echo "extracting catalogue application"
 unzip /tmp/catalogue.zip &>>$LOG_FILE
 if [ $? -eq 0 ]
   then
-  echo -e Status = "\2[32mSUCCESS\e[0m"
+  echo -e Status = "\e[32mSUCCESS\e[0m"
   else
   echo -e Status = "\e[31mFAILURE\e[0m"
   exit 1
@@ -70,7 +71,7 @@ echo "installing nodejs dependencies"
 npm install &>>$LOG_FILE
 if [ $? -eq 0 ]
   then
-  echo -e Status = "\2[32mSUCCESS\e[0m"
+  echo -e Status = "\e[32mSUCCESS\e[0m"
   else
   echo -e Status = "\e[31mFAILURE\e[0m"
   exit 1
@@ -80,7 +81,7 @@ echo "setup catalogue service file"
 mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>$LOG_FILE
 if [ $? -eq 0 ]
   then
-  echo -e Status = "\2[32mSUCCESS\e[0m"
+  echo -e Status = "\e[32mSUCCESS\e[0m"
   else
   echo -e Status = "\e[31mFAILURE\e[0m"
   exit 1
@@ -93,7 +94,7 @@ echo "starting catalogue service"
 systemctl restart catalogue &>>$LOG_FILE
 if [ $? -eq 0 ]
   then
-  echo -e Status = "\2[32mSUCCESS\e[0m"
+  echo -e Status = "\e[32mSUCCESS\e[0m"
   else
   echo -e Status = "\e[31mFAILURE\e[0m"
   exit 1
