6428334713e8b52a4c3b05404a86cb29b7d21f84
ACER <manirocks43@gmail.com>
2022-09-20 22:22:52 -0400

    Functions

--- catalogue.sh
+++ catalogue.sh
@@ -7,103 +7,59 @@ if [ $ID -ne 0 ]
   exit 1
 fi
 
-echo "setup nodejs repo"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOG_FILE
-if [ $? -eq 0 ]
-  then
-  echo -e Status = "\e[32mSUCCESS\e[0m"
-  else
-  echo -e Status = "\e[31mFAILURE\e[0m"
-  exit 1
-fi
-
-echo "installing nodejs repo"
-yum install nodejs -y &>>$LOG_FILE
-if [ $? -eq 0 ]
-  then
-  echo -e Status = "\e[32mSUCCESS\e[0m"
-  else
-  echo -e Status = "\e[31mFAILURE\e[0m"
-  exit 1
-fi
-
-id roboshop &>>$LOG_FILE
-if [ $? -ne 0 ] ; then
-  echo "Adding roboshop user"
-  useradd roboshop &>>$LOG_FILE
-  if [ $? -eq 0 ]
+StatusCheck () {
+  if [ $1 -eq 0 ]
     then
     echo -e Status = "\e[32mSUCCESS\e[0m"
     else
     echo -e Status = "\e[31mFAILURE\e[0m"
     exit 1
   fi
+}
+
+echo "setup nodejs repo"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOG_FILE
+StatusCheck $?
+
+echo "installing nodejs repo"
+yum install nodejs -y &>>$LOG_FILE
+StatusCheck $?
+
+id roboshop &>>$LOG_FILE
+if [ $? -ne 0 ] ; then
+  echo "Adding roboshop user"
+  useradd roboshop &>>$LOG_FILE
+  StatusCheck $?
 fi
 
 echo "downloading catalogue application"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG_FILE
-if [ $? -eq 0 ]
-  then
-  echo -e Status = "\e[32mSUCCESS\e[0m"
-  else
-  echo -e Status = "\e[31mFAILURE\e[0m"
-  exit 1
-fi
+StatusCheck $?
 
 cd /home/roboshop
 
 echo "Cleaning old content"
 rm -rf catalogue &>>$LOG_FILE
-if [ $? -eq 0 ]
-  then
-  echo -e Status = "\e[32mSUCCESS\e[0m"
-  else
-  echo -e Status = "\e[31mFAILURE\e[0m"
-  exit 1
-fi
+StatusCheck $?
 
 echo "extracting catalogue application"
 unzip /tmp/catalogue.zip &>>$LOG_FILE
-if [ $? -eq 0 ]
-  then
-  echo -e Status = "\e[32mSUCCESS\e[0m"
-  else
-  echo -e Status = "\e[31mFAILURE\e[0m"
-  exit 1
-fi
+StatusCheck $?
 
 mv catalogue-main catalogue
 cd /home/roboshop/catalogue
 
 echo "installing nodejs dependencies"
 npm install &>>$LOG_FILE
-if [ $? -eq 0 ]
-  then
-  echo -e Status = "\e[32mSUCCESS\e[0m"
-  else
-  echo -e Status = "\e[31mFAILURE\e[0m"
-  exit 1
-fi
+StatusCheck $?
 
 echo "setup catalogue service file"
 mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>$LOG_FILE
-if [ $? -eq 0 ]
-  then
-  echo -e Status = "\e[32mSUCCESS\e[0m"
-  else
-  echo -e Status = "\e[31mFAILURE\e[0m"
-  exit 1
-fi
+StatusCheck $?
 
 systemctl daemon-reload &>>$LOG_FILE
 systemctl enable catalogue &>>$LOG_FILE
 
 echo "starting catalogue service"
 systemctl restart catalogue &>>$LOG_FILE
-if [ $? -eq 0 ]
-  then
-  echo -e Status = "\e[32mSUCCESS\e[0m"
-  else
-  echo -e Status = "\e[31mFAILURE\e[0m"
-  exit 1
-fi
+StatusCheck $?
