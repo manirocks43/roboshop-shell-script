e26929764db8a926fad4a4fe5716ec8c610b6e7b
ACER <manirocks43@gmail.com>
2022-09-21 21:37:56 -0400

    updates

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,7 @@
 LOG_FILE=/tmp/mysql
 source common.sh
 
-echo"Setup mysql Repo file"
+echo "Setup mysql Repo file"
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>LOG_FILE
 StatusCheck $?
 
