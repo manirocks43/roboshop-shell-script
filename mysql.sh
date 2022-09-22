6d20d13062f3977e3da6fb4f4e3f31cd6f8bf4c1
ACER <manirocks43@gmail.com>
2022-09-21 21:53:42 -0400

    updates

--- mysql.sh
+++ mysql.sh
@@ -3,18 +3,18 @@ LOG_FILE=/tmp/mysql
 source common.sh
 
 echo "Setup mysql Repo file"
-curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>LOG_FILE
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>$LOG_FILE
 StatusCheck $?
 
 echo "Disable Mysql default module to enable 5.7 Mysql"
-dnf module disable mysql -y &>>LOG_FILE
+dnf module disable mysql -y &>>$LOG_FILE
 StatusCheck $?
 
 echo "Install Mysql"
-yum install mysql-community-server -y &>>LOG_FILE
+yum install mysql-community-server -y &>>$LOG_FILE
 StatusCheck $?
 
 echo "start mysql service"
-systemctl enable mysqld &>>LOG_FILE
-systemctl restart mysqld &>>LOG_FILE
+systemctl enable mysqld &>>$LOG_FILE
+systemctl restart mysqld &>>$LOG_FILE
 StatusCheck $?
