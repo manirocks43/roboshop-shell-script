efd5439898d6e7c9734a8954ae8791084580d3df
ACER <manirocks43@gmail.com>
2022-09-26 23:00:23 -0400

    updates

---
+++ dispatch.sh
@@ -0,0 +1,23 @@
+COMPONENT=dispatch
+LOG_FILE=/tmp/$COMPONENT
+
+source common.sh
+
+echo "installing golang application"
+yum install golang -y &>>$LOG_FILE
+StatusCheck $?
+
+APP_PREREQ
+
+echo "initiate, get and build golang app server"
+go mod init dispatch &>>$LOG_FILE
+StatusCheck $?
+go get &>>$LOG_FILE
+StatusCheck $?
+go build &>>$LOG_FILE
+StatusCheck $?
+
+
+
+
+
