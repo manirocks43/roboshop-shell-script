32887a28213f45c6b0c8dcb189aba650373c8ff4
ACER <manirocks43@gmail.com>
2022-09-27 19:12:04 -0400

    updates

--- frontend.sh
+++ frontend.sh
@@ -2,28 +2,28 @@ LOG_FILE=/tmp/frontend
 
 source common.sh
 
-echo Installing Nginx
+echo "Installing Nginx"
 yum install nginx -y
 StatusCheck $?
 
-echo Downloading Nginx WebContent
+echo "Downloading Nginx WebContent"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG_FILE
 StatusCheck $?
 
 cd /usr/share/nginx/html
 
-echo Removing old web content
+echo "Removing old web content"
 rm -rf * &>>$LOG_FILE
 StatusCheck $?
 
-echo Extracting WebContent
+echo "Extracting WebContent"
 unzip /tmp/frontend.zip &>>$LOG_FILE
 StatusCheck $?
 
 mv frontend-main/static/* .
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
 
-echo Starting Nginx service
+echo "Starting Nginx service"
 systemctl enable nginx &>>$LOG_FILE
 systemctl restart nginx &>>$LOG_FILE
 StatusCheck $?
